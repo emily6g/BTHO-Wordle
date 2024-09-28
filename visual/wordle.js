01cb14f04f407ac0560b3dc7621dbbf015c3c1d5
isabellaguajardo <inanetteg23@gmail.com>
2024-09-28 16:48:53 -0500

    hello

--- visual/wordle.js
+++ visual/wordle.js
@@ -1,16 +1,13 @@
 function initBoard() {
-    let board = document.getElementById("game-board");
-
+    let board = document.getElementById("gameboard");
     for (let i = 0; i < 6; i++) {
         let row = document.createElement("div")
         row.className = "letter-row"
-
         for (let j = 0; j < 5; j++) {
             let box = document.createElement("div")
             box.className = "letter-box"
             row.appendChild(box)
         }
-
         board.appendChild(row)
     }
 }
