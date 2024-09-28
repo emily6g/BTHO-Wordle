2e57412925ecd566e82d53ebe3fb65433f6d50c4
arpita-a04 <arpitaa04@tamu.edu>
2024-09-28 17:40:22 -0500

    newchange

--- visual/wildwest.js
+++ visual/wildwest.js
@@ -3,3 +3,18 @@
 const Wildwest_wordbank = ["Boots", "Horse",];
 
 
+function initBoard() {
+    let board = document.getElementById("gameboard");
+    for (let i = 0; i < 6; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+        board.appendChild(row)
+    }
+}
+
+initBoard()
