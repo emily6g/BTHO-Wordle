f21fd11669ed46eb316dd4454ce0f744e2423691
isabellaguajardo <inanetteg23@gmail.com>
2024-09-28 17:50:09 -0500

    hello

--- visual/wordle.js
+++ visual/wordle.js
@@ -1,3 +1,10 @@
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+//let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+//console.log(rightGuessString)
+
 function initBoard() {
     let board = document.getElementById("gameboard");
     for (let i = 0; i < 6; i++) {
@@ -13,3 +20,50 @@ function initBoard() {
 }
 
 initBoard()
+
+document.addEventListener('keyup', (e)=>{
+    if (guessesRemaining === 0) {
+        return
+    }
+    
+    let pressedKey = String(e.key)
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+    
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+    
+    let found = pressedKey.match(/[a-z]/gi)
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+function insertLetter (pressedKey) {
+    if (nextLetter === 5) {
+      return
+    }
+    
+    pressedKey = pressedKey.toLowerCase()
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter]
+    box.textContent = pressedKey
+    box.classList.add("filled-box")
+    currentGuess.push(pressedKey)
+    nextLetter += 1
+}
+
+function deleteLetter(){
+    let row = document.getElementsByClassName("letter-row")[6- guessesRemaining]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    currentGuess.pop()
+    nextLetter -=1
+}
