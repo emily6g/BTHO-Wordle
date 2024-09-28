099b581e1a151ec94180ef6bd61edd2578828989
isav06 <isaevictor06@gmail.com>
2024-09-28 14:44:05 -0500

    Update howdy.py

--- howdy.py
+++ howdy.py
@@ -1,9 +1,7 @@
 #heyy guys this is code
 print("howdy")
 print(456)
-<<<<<<< HEAD
 print("hii")
-=======
 
 #hey guys can yall see this?? - Arpita
->>>>>>> 6af4cc70d579726a907f21c1832c92b4c52a0373
+#hi i am adding this 
