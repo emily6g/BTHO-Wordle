a7e2fe90e935d17f8e4c7c07a8a282730d992267
isav06 <isaevictor06@gmail.com>
2024-09-29 00:38:30 -0500

    testing

--- bthowordle.py
+++ bthowordle.py
@@ -1,5 +1,3 @@
-
-
 import random
 
 # Greets user, explains the rules
--- visual/unlimited/unlimited.js
+++ visual/unlimited/unlimited.js
@@ -11,7 +11,7 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let guessWord = words[Math.floor(Math.random() * words.length)]
+let guessWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
 console.log(guessWord)
 let letters = guessWord.length
 
@@ -30,4 +30,107 @@ function initBoard() {
     }
 }
 
-initBoard()
+initBoard();
+
+document.addEventListener('keyup', (e) => {
+    if (guessesRemaining === 0) {
+        return;
+    }
+
+
+    let pressedKey = String(e.key);
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter();
+        return;
+    }
+
+
+    if (pressedKey === "Enter") {
+        checkGuess();
+        return;
+    }
+
+
+    let found = pressedKey.match(/[a-z]/gi);
+    if (!found || found.length > 1) {
+        return;
+    } else {
+        insertLetter(pressedKey);
+    }
+});
+
+function insertLetter(pressedKey) {
+    if (nextLetter === letters) {
+        return;
+    }
+
+
+    pressedKey = pressedKey.toLowerCase();
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let box = row.children[nextLetter];
+    box.textContent = pressedKey;
+    box.classList.add("filled-box");
+    currentGuess.push(pressedKey);
+    nextLetter += 1;
+}
+
+
+function deleteLetter() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let box = row.children[nextLetter - 1];
+    box.textContent = "";
+    box.classList.remove("filled-box");
+    currentGuess.pop();
+    nextLetter -= 1;
+}
+
+
+function checkGuess() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let guessString = currentGuess.join('');
+    let rightGuess = Array.from(guessWord);
+
+
+    if (guessString.length !== letters) {
+        alert("Not enough letters!");
+        return;
+    }
+
+
+    for (let i = 0; i < letters; i++) {
+        let box = row.children[i];
+        let letter = currentGuess[i];
+        let letterColor = '';
+
+
+        if (rightGuess[i] === letter) {
+            letterColor = 'green';
+        } else if (rightGuess.includes(letter)) {
+            letterColor = 'yellow';
+        } else {
+            letterColor = 'grey';
+        }
+
+
+        setTimeout(() => {
+            box.classList.add(letterColor);
+        }, 250 * i);
+    }
+
+
+    if (guessString === guessWord) {
+        alert("You guessed right! Game over!");
+        guessesRemaining = 0;
+        return;
+    } else {
+        guessesRemaining -= 1;
+        currentGuess = [];
+        nextLetter = 0;
+
+
+        if (guessesRemaining === 0) {
+            alert(`You've run out of guesses! The correct word was: "${guessWord}"`);
+        }
+    }
+}
+#woooo
