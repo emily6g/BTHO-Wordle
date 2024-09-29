d281e8103660ee0651ab7623c7a99f92d4b041bd
isav06 <isaevictor06@gmail.com>
2024-09-29 09:53:28 -0500

    hi

--- visual/wordle/wordle.js
+++ visual/wordle/wordle.js
@@ -1,5 +1,5 @@
 const words = [
-    "Whoop", "Howdy", "Gigem", "Corps", "Aggie", "Spirit", "Honor", "Study", "Yells",
+    "Whoop", "Howdy", "Gigem", "Corps", "Aggie", "Honor", "Study", "Yells",
     "Pride", "Loyal", "Games", "Evans", "Annex", "Sbisa", "Unity", "Class", "Panda",
     "Trust", "Texas", "Cadet", "Fight", "Final", "Alumn", "Dorms", "Rings"
 ];
