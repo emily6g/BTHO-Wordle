cc8e0cb933167a9ba4d0e223f8bc5dc02b6a8d89
arpita-a04 <arpitaa04@tamu.edu>
2024-09-29 01:07:54 -0500

    ignore

--- visual/wildwest/wildwest.js
+++ visual/wildwest/wildwest.js
@@ -1,20 +1,134 @@
+const words = [ "Boots", "Horse","Howdy","Texas","Ranch","Lasso","Rodeo","Trail","Spurs","Cacti","Field","Barns","Farms","Wagon","Dusty","Shoot","Ropes","Sheep","Goats", "Burro","Rusty","Denim", "Sling","Wrang","Bison","Mules"
+    
+];
 
-/*create a string of words here for a word bank*/
-const Wildwest_wordbank = ["Boots", "Horse",];
+
+const NUMBER_OF_GUESSES = 6;
+let guessesRemaining = NUMBER_OF_GUESSES;
+let currentGuess = [];
+let nextLetter = 0;
+let guessWord = words[Math.floor(Math.random() * words.length)].toLowerCase();
+console.log(guessWord);
 
 
 function initBoard() {
     let board = document.getElementById("gameboard");
     for (let i = 0; i < 6; i++) {
-        let row = document.createElement("div")
-        row.className = "letter-row"
+        let row = document.createElement("div");
+        row.className = "letter-row";
         for (let j = 0; j < 5; j++) {
-            let box = document.createElement("div")
-            box.className = "letter-box"
-            row.appendChild(box)
+            let box = document.createElement("div");
+            box.className = "letter-box";
+            row.appendChild(box);
         }
-        board.appendChild(row)
+        board.appendChild(row);
     }
 }
 
-initBoard()
+
+initBoard();
+
+
+document.addEventListener('keyup', (e) => {
+    if (guessesRemaining === 0) {
+        return;
+    }
+
+
+    let pressedKey = String(e.key);
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter();
+        return;
+    }
+
+
+    if (pressedKey === "Enter") {
+        checkGuess();
+        return;
+    }
+
+
+    let found = pressedKey.match(/[a-z]/gi);
+    if (!found || found.length > 1) {
+        return;
+    } else {
+        insertLetter(pressedKey);
+    }
+});
+
+
+function insertLetter(pressedKey) {
+    if (nextLetter === 5) {
+        return;
+    }
+
+
+    pressedKey = pressedKey.toLowerCase();
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let box = row.children[nextLetter];
+    box.textContent = pressedKey;
+    box.classList.add("filled-box");
+    currentGuess.push(pressedKey);
+    nextLetter += 1;
+}
+
+
+function deleteLetter() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let box = row.children[nextLetter - 1];
+    box.textContent = "";
+    box.classList.remove("filled-box");
+    currentGuess.pop();
+    nextLetter -= 1;
+}
+
+
+function checkGuess() {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining];
+    let guessString = currentGuess.join('');
+    let rightGuess = Array.from(guessWord);
+
+
+    if (guessString.length !== 5) {
+        alert("Not enough letters!");
+        return;
+    }
+
+
+    for (let i = 0; i < 5; i++) {
+        let box = row.children[i];
+        let letter = currentGuess[i];
+        let letterColor = '';
+
+
+        if (rightGuess[i] === letter) {
+            letterColor = 'green';
+        } else if (rightGuess.includes(letter)) {
+            letterColor = 'yellow';
+        } else {
+            letterColor = 'grey';
+        }
+
+
+        setTimeout(() => {
+            box.classList.add(letterColor);
+        }, 250 * i);
+    }
+
+
+    if (guessString === guessWord) {
+        alert("You guessed right! Game over!");
+        guessesRemaining = 0;
+        return;
+    } else {
+        guessesRemaining -= 1;
+        currentGuess = [];
+        nextLetter = 0;
+
+
+        if (guessesRemaining === 0) {
+            alert(`You've run out of guesses! The correct word was: "${guessWord}"`);
+        }
+    }
+}
+
