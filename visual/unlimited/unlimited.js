c933250d60b1435575f5c3697ea1a6e24e7517b4
isabellaguajardo <inanetteg23@gmail.com>
2024-09-29 04:33:00 -0500

    gvbhnj

--- visual/unlimited/unlimited.js
+++ visual/unlimited/unlimited.js
@@ -180,6 +180,31 @@ function closeCustomAlert() {
         spread: 120,
         startVelocity: 45,
     });
+
+    var end = Date.now() + (15 * 1000);
+
+    var colors = ['#bb0000', '#ffffff'];
+
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
 }
 
   function showCustomAlert2() {
--- visual/wildwest/wildwest.js
+++ visual/wildwest/wildwest.js
@@ -142,6 +142,31 @@ function showCustomAlert() {
 function closeCustomAlert() {
     var alertModal = document.getElementById("customAlert");
     alertModal.style.display = "none";
+
+    var end = Date.now() + (15 * 1000);
+
+    var colors = ['#bb0000', '#ffffff'];
+
+    (function frame() {
+        confetti({
+            particleCount: 2,
+            angle: 60,
+            spread: 55,
+            origin: { x: 0 },
+            colors: colors
+        });
+        confetti({
+            particleCount: 2,
+            angle: 120,
+            spread: 55,
+            origin: { x: 1 },
+            colors: colors
+        });
+
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    }());
   }
 
   function showCustomAlert2() {
